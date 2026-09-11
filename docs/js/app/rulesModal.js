bfe62b8b7822d16f476ba803ee874d8fcd028402
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-11 12:21:19 -0400

    Update rulesModal.js

--- docs/js/app/rulesModal.js
+++ docs/js/app/rulesModal.js
@@ -2,6 +2,7 @@ import { appState } from "./state.js";
 import { acceptRules } from "../services/userService.js";
 import { showTab } from "./tabs.js";
 import { POOL_CONFIG } from "../constants.js";
+import { formatDeadline} from "../functions.js";
 
 export function showRulesModal() {
 
