2308d1c06a30c1893481393831d07e8d432bebff
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-14 14:35:06 -0400

    Update renderResults.js

--- docs/js/ui/results/renderResults.js
+++ docs/js/ui/results/renderResults.js
@@ -3,7 +3,7 @@ import { getAllPredictions} from "../../services/firestoreService.js";
 import { computeLeaderboard, getRoundFromKey } from "../../logic/scoring.js";
 import { getParentMatch, isResultAvailable } from "../../utils/helpers.js";
 import { getRound1Matchups } from "../../services/matchService.js";
-import { SCORING, MATCH_ORDER } from "../../constants.js";
+import { SCORING, MATCH_ORDER,getTeamLogo } from "../../constants.js";
 
 
 export async function loadPredictionsDetails() {
